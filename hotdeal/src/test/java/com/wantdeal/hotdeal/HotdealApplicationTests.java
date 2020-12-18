b4f1e4e1890ff326c42b4bdbfb09a2226db2c29a
mdjuniooor <mmdsds@u.sogang.ac.kr>
2020-12-18 22:00:21 +0900

    Initial Config Server Setting

---
+++ hotdeal/src/test/java/com/wantdeal/hotdeal/HotdealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wantdeal.hotdeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotdealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
