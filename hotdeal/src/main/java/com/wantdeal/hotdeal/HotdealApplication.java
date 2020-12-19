36bcb1c3d217c233e4613c7a6e95a4910c28d8a7
mdjuniooor <mmdsds@u.sogang.ac.kr>
2020-12-19 12:45:55 +0900

    Refactoring Pathing Config Using Eureka

--- hotdeal/src/main/java/com/wantdeal/hotdeal/HotdealApplication.java
+++ hotdeal/src/main/java/com/wantdeal/hotdeal/HotdealApplication.java
@@ -2,7 +2,9 @@ package com.wantdeal.hotdeal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class HotdealApplication {
 
