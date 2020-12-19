fde87f820c0591b7ae9f07117bbe25c116d0c6cd
mdjuniooor <mmdsds@u.sogang.ac.kr>
2020-12-19 12:03:04 +0900

    Setting Gateway & Create Eureka

---
+++ gateway/src/main/java/com/hotdeal/gateway/filters/GatewayErrorFilter.java
@@ -0,0 +1,32 @@
+package com.hotdeal.gateway.filters;
+
+
+import com.netflix.zuul.ZuulFilter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class GatewayErrorFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return FilterConstants.ERROR_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        log.info("Using Error Filter");
+        return null;
+    }
+}
